77698d565d9903c3e4db2febf23b9ad18b524dcd
nathamfernandes <natham.fr@gmail.com>
2024-12-08 11:06:41 -0300

    chore: alteracao nome do grupo e da aplicação

--- src/main/java/com/projetolp2/mesafacil/MesafacilApplication.java
+++ src/main/java/com/projetolp2/mesafacil/MesafacilApplication.java
@@ -1,4 +1,4 @@
-package com.mesafacil.todosimple;
+package com.projetolp2.mesafacil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/projetolp2/mesafacil/MesafacilApplicationTests.java
+++ src/test/java/com/projetolp2/mesafacil/MesafacilApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mesafacil.todosimple;
+package com.projetolp2.mesafacil;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
