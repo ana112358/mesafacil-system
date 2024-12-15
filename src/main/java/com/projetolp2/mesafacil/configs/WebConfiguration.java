03b87a51ba0d7f3c8b40a6ff1b32c8ece9a85913
nathamfernandes <natham.fr@gmail.com>
2024-12-15 10:15:33 -0300

    feat: servindo arquivos estáticos do front-end com o Spring Boot

--- src/main/java/com/projetolp2/mesafacil/configs/WebConfiguration.java
+++ src/main/java/com/projetolp2/mesafacil/configs/WebConfiguration.java
@@ -2,13 +2,19 @@ package com.projetolp2.mesafacil.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer{
+public class WebConfiguration implements WebMvcConfigurer {
+
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("/public", "classpath:/static/");
     }
 }
---
+++ src/main/java/com/projetolp2/mesafacil/controllers/MainController.java
@@ -0,0 +1,13 @@
+package com.projetolp2.mesafacil.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/index.html";
+    }
+}
