546936646d90fcbe31c65c40fd36d744f17122b6
SebastiaoFellipe <sebaslopes369@gmail.com>
2024-12-15 02:12:44 -0300

    feat: adicionar rota GET para retornar todas as reservas

--- src/main/java/com/projetolp2/mesafacil/controllers/ReservaController.java
+++ src/main/java/com/projetolp2/mesafacil/controllers/ReservaController.java
@@ -1,21 +1,18 @@
 package com.projetolp2.mesafacil.controllers;
 
 import java.net.URI;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import com.projetolp2.mesafacil.models.Reserva;
 import com.projetolp2.mesafacil.models.Reserva.UpdateReserva;
 import com.projetolp2.mesafacil.models.Reserva.CreateReserva;
 import com.projetolp2.mesafacil.services.ReservaService;
-
 import jakarta.validation.Valid;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,11 +21,17 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
-@RequestMapping("/reserva")
+@RequestMapping("/reservas")
 @Validated
 public class ReservaController {
     @Autowired
     private ReservaService reservaService;
+
+    @GetMapping
+    public ResponseEntity<List<Reserva>> findAll() {
+        List<Reserva> lista = this.reservaService.findAll();
+        return ResponseEntity.ok().body(lista);
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<Reserva> findById(@PathVariable Integer id) {
