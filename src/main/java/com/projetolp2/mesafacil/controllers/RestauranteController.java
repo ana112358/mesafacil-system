5b2d924a5bf22648f437329f32cfe8db76a30f59
SebastiaoFellipe <sebaslopes369@gmail.com>
2024-12-15 02:18:59 -0300

    refactor: altera o nome da rota de Restaurante

--- src/main/java/com/projetolp2/mesafacil/controllers/RestauranteController.java
+++ src/main/java/com/projetolp2/mesafacil/controllers/RestauranteController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/restaurantes")
+@RequestMapping("/restaurantes")
 public class RestauranteController {
 
     @Autowired
